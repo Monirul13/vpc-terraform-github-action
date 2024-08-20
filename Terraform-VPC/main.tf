2458b28e516cf8834ea6bfa6214f116eae1c764f
rana <rana@rana.myguest.virtualbox.org>
2024-08-20 18:38:15 +0600

    ec2 instance and alb fixed

--- Terraform-VPC/main.tf
+++ Terraform-VPC/main.tf
@@ -13,18 +13,18 @@ module "sg" {
 
 
 
-# module "ec2" {
-#   source = "./modules/ec2"
-#   sg_id = module.sg.sg_id
-#   subnets = module.vpc.subnet_ids
-# }
+module "ec2" {
+  source = "./modules/ec2"
+  sg_id = module.sg.sg_id
+  subnets = module.vpc.subnet_ids
+}
 
 
 
-# module "alb" {
-#   source = "./modules/alb"
-#   sg_id = module.sg.sg_id
-#   subnets = module.vpc.subnet_ids 
-#   vpc_id = module.vpc.vpc_id
-#   instances = module.ec2.instances
-# }
+module "alb" {
+  source = "./modules/alb"
+  sg_id = module.sg.sg_id
+  subnets = module.vpc.subnet_ids 
+  vpc_id = module.vpc.vpc_id
+  instances = module.ec2.instances
+}
